23a259bf891200d285eced03692682d8372d31e5
Madhusudhan Sastry <magundu@deloitte.com>
2018-07-14 15:44:42 +0530

    added ribbon routing through Eureka service discovery

--- ZuulGateway/src/main/java/com/charan/zuul/ZuulGatewayApplication.java
+++ ZuulGateway/src/main/java/com/charan/zuul/ZuulGatewayApplication.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("com.charan.*")
 public class ZuulGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZuulGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayApplication.class, args);
+    }
 }
